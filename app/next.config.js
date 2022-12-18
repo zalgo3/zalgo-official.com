6845872bd5fda67f458a1bfe1af420c3d1f0483f
zalgo <zalgo3333@gmail.com>
2022-12-18 12:57:07 +0900

    chore: add basePath to next.config.js
    This is needed to deploy the app to a subdirectory on the server.

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
+    swcMinify: true,
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/zalgo-official.com',
 };
 
 module.exports = nextConfig;
