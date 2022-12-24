c98520079e171cddf2d45fc372600286159720f2
zalgo <zalgo3333@gmail.com>
2022-12-24 17:27:52 +0900

    feat: add link to blog posts
    This commit adds a link to the blog posts.
    
    The link is added to the card header.

--- app/app/blog/DisplayPosts.tsx
+++ app/app/blog/DisplayPosts.tsx
@@ -2,19 +2,20 @@
 
 import { type PostData } from "lib/posts";
 import { useStyles } from "lib/styles";
+import { Link } from "@fluentui/react-components";
 import { Card, CardHeader } from "@fluentui/react-components/unstable";
 
 const DisplayPosts = ({ posts }: { posts: PostData[] }) => {
   const styles = useStyles();
-  return (
-    <>
+  return <>
       {posts.map((post: PostData) => (
-        <Card className={styles.card} as="a" href={`/blog/${post.slug}`}>
-          <CardHeader header={post.title} description={post.createdAt} />
+        <Card className={styles.card}>
+          <Link href={`/blog/${post.slug}`}>
+            <CardHeader header={post.title} description={post.createdAt} />
+          </Link>
         </Card>
       ))}
-    </>
-  );
+    </>;
 };
 
 export default DisplayPosts;
