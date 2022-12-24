f88816e577f534b729db74c8c990ab27cf55520d
zalgo <zalgo3333@gmail.com>
2022-12-24 17:40:34 +0900

    Remove unused post property
    The post property `_` was unused and was causing a warning to be
    displayed in the console.

--- app/app/blog/[slug]/page.tsx
+++ app/app/blog/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { serialize } from "next-mdx-remote/serialize";
 import DisplayPost from "./DisplayPost";
 
 const Page = async ({ params }: { params: { slug: string } }) => {
-  const { content, ...post } = await getPost(params.slug);
+  const { content, _ } = await getPost(params.slug);
   const source = await serialize(content, {mdxOptions: {development: false}});
   return <DisplayPost source={source} />;
 };
