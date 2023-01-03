0cd55180d8b55fd1d1bb7cb23b39344210117591
zalgo3 <zalgo3333@gmail.com>
2023-01-03 20:29:31 +0900

    Fix typo in blog post
    The blog post was missing a const keyword.

--- app/app/blog/page.tsx
+++ app/app/blog/page.tsx
@@ -5,7 +5,7 @@ import React, {useState, useEffect} from 'react';
 import {getPostDataAll} from 'lib/posts';
 
 const Page = async () => {
-    posts = await getData();
+    const posts = await getData();
     return (
         <>
             <Header />
