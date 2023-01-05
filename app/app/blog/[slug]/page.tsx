ff047ffb621c4a0e874107f1d7bfff7b1077bb3f
zalgo <zalgo3333@gmail.com>
2023-01-05 22:22:01 +0900

    Support KaTeX

--- app/app/blog/[slug]/page.tsx
+++ app/app/blog/[slug]/page.tsx
@@ -4,29 +4,20 @@ import Article from './article';
 import Header from 'app/blog/header';
 import Footer from 'app/footer';
 import remarkMath from 'remark-math';
-import rehypePrism from '@mapbox/rehype-prism';
 import rehypeKatex from 'rehype-katex';
 
 const Page = async ({params}: {params: {slug: string}}) => {
     const {content, ...post} = getPost(params.slug);
     const source = await serialize(content, {
         mdxOptions: {
-            development: false,
             remarkPlugins: [remarkMath],
             rehypePlugins: [rehypeKatex],
-        },
-    });
-    const mathSource = await serialize('Hello *world* and $1+1=2$ math mode', {
-        mdxOptions: {
             development: false,
-            remarkPlugins: [remarkMath],
-            rehypePlugins: [rehypeKatex],
         },
     });
     return (
         <>
             <Header />
-            <Article source={mathSource} />
             <Article source={source} />
             <Footer />
         </>
