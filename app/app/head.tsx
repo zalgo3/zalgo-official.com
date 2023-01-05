ef536b0f514c4f89d8b420be43bd5b7c98298c3d
zalgo <zalgo3333@gmail.com>
2023-01-05 22:24:08 +0900

    prettier

--- app/app/head.tsx
+++ app/app/head.tsx
@@ -3,7 +3,13 @@ const Head = () => {
         <>
             <title>ざるご / 田辺広樹</title>
             <meta name="robots" content="noindex" />
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0" crossorigin="anonymous" precedence="default"/>
+            <link
+                rel="stylesheet"
+                href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css"
+                integrity="sha384-vKruj+a13U8yHIkAyGgK1J3ArTLzrFGBbBc0tDp4ad/EyewESeXE/Iv67Aj8gKZ0"
+                crossorigin="anonymous"
+                precedence="default"
+            />
         </>
     );
 };
