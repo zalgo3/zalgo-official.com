45f7cec0f1ecea624988df62b1d64859437eb6de
zalgo <zalgo3333@gmail.com>
2023-02-12 22:58:07 +0900

    Add a container to DisplayPosts
    The container is a flexbox that wraps the cards and centers them.

--- app/app/blog/DisplayPosts.tsx
+++ app/app/blog/DisplayPosts.tsx
@@ -2,14 +2,15 @@
 
 import { type PostData } from 'lib/posts';
 import Card from './card';
+import styles from 'styles/app/blog/DisplayPosts.module.css';
 
 const DisplayPosts = ({ posts }: { posts: PostData[] }) => {
     return (
-        <>
+        <div className={styles.container}>
             {posts.map((post: PostData) => (
                 <Card post={post} />
             ))}
-        </>
+        </div>
     );
 };
 
