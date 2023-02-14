067da41c7aa64906777dbd5145930250e7ef5f5f
zalgo <zalgo3333@gmail.com>
2023-02-15 01:46:49 +0900

    fix post titles

--- app/app/blog/card.tsx
+++ app/app/blog/card.tsx
@@ -1,13 +1,22 @@
-import {type PostData} from 'lib/posts';
+import { type PostData } from 'lib/posts';
 import Link from 'next/link';
 import styles from 'styles/app/blog/card.module.css';
-import {utcToZonedTime, format as formatTZ} from 'date-fns-tz';
+import { utcToZonedTime, format as formatTZ } from 'date-fns-tz';
 
-const Card = ({post}: {post: PostData}) => {
+const truncateTitle = (title: string) => {
+    const maxLength = 52;
+    if (title.length > maxLength) {
+        return title.slice(0, maxLength) + '...';
+    } else {
+        return title;
+    }
+};
+
+const Card = ({ post }: { post: PostData }) => {
     return (
         <article className={styles.card}>
             <Link href={`/blog/${post.slug}`} key={post.slug}>
-                <h2>{post.title}</h2>
+                <h2>{truncateTitle(post.title)}</h2>
                 <footer>
                     {formatTZ(
                         utcToZonedTime(post.createdAt * 1000, 'Asia/Tokyo'),
