26173dc90a586626c30de93dd379881a2dddb663
zalgo <zalgo3333@gmail.com>
2023-03-07 02:33:27 +0900

    .

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -29,12 +29,4 @@ export default {
         }
         return config;
     },
-    webpackDevMiddleware: config => {
-        config.watchOptions = {
-            poll: 5000,
-            aggregateTimeout: 300,
-        }
-
-        return config
-    },
 };
