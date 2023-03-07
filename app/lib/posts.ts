ad0c95863b8e0d4c859e3ba89e0c7391002fdf77
zalgo <zalgo3333@gmail.com>
2023-03-07 22:01:54 +0900

    Revert "Use import { promises as fs } from fs"
    This reverts commit 6122b1c5624783c151e304e1181119bc78f1f479.

--- app/lib/posts.ts
+++ app/lib/posts.ts
@@ -1,5 +1,5 @@
 import {exec} from 'child_process';
-import {promises as fs} from 'fs';
+import fs from 'fs/promises';
 import matter, {GrayMatterFile, Input} from 'gray-matter';
 import path from 'path';
 import {promisify} from 'util';
