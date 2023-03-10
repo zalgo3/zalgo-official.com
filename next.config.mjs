f5215ba82908c60697a3556be42cf68d707589b1
zalgo3 <zalgo3333@gmail.com>
2023-03-10 13:58:46 +0900

    do not use pa-api

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 import path from 'path';
 
 export default {
-    staticPageGenerationTimeout: 3600,
     reactStrictMode: true,
     swcMinify: true,
     experimental: {
--- ui/affiliates.tsx
+++ ui/affiliates.tsx
@@ -3,101 +3,35 @@ import fetch from 'node-fetch';
 import {truncateTitle} from 'lib/string';
 import Image from 'next/image';
 import Link from 'next/link';
-import ProductAdvertisingAPIv1 from 'paapi5-nodejs-sdk';
 import styles from 'styles/ui/affiliates.module.css';
 
-type AmazonItem = {
-    DetailPageURL: string;
-    Images: {
-        Primary: {
-            Medium: {
-                URL: string;
-            };
-        };
-    };
-    ItemInfo: {
-        Title: {
-            DisplayValue: string;
-        };
-        ExternalIds?: {
-            EANs: {
-                DisplayValues: string[];
-                Label: string;
-                Locale: string;
-            };
-            ISBNs: {
-                DisplayValues: string[];
-                Label: string;
-                Locale: string;
-            };
-            UPCs: {
-                DisplayValues: string[];
-                Label: string;
-                Locale: string;
-            };
-        };
-    };
+type RakutenItem = {
+    affiliateUrl: string;
+    itemName: string;
+    mediumImageUrls: string[];
 };
 
-const getAmazonItem = async (
+const getRakutenItem = async (
     query: string,
-    asin?: string
-): Promise<AmazonItem> => {
-    const defaultClient = ProductAdvertisingAPIv1.ApiClient.instance;
-    defaultClient.accessKey = process.env.AMAZON_PA_API_ACCESS_KEY_ID;
-    defaultClient.secretKey = process.env.AMAZON_PA_API_SECRET_KEY;
-    defaultClient.host = 'webservices.amazon.co.jp';
-    defaultClient.region = 'us-west-2';
-    const api = new ProductAdvertisingAPIv1.DefaultApi();
-    const request =
-        asin != null
-            ? new ProductAdvertisingAPIv1.GetItemsRequest()
-            : new ProductAdvertisingAPIv1.SearchItemsRequest();
-    if (asin) {
-        request.ItemIds = [asin];
-    } else {
-        request.Keywords = query;
-        request.ItemCount = 1;
-    }
-    request.PartnerTag = process.env.AMAZON_ASSOCIATE_PARTNER_TAG;
-    request.PartnerType = 'Associates';
-    request.Resources = [
-        'Images.Primary.Medium',
-        'ItemInfo.Title',
-        'ItemInfo.ExternalIds',
-        'Offers.Listings.Price',
-    ];
-    const response =
-        asin != null
-            ? await api.getItems(request)
-            : await api.searchItems(request);
-    const item =
-        asin != null
-            ? response.ItemsResult.Items[0]
-            : response.SearchResult.Items[0];
-    return {
-        DetailPageURL: item.DetailPageURL,
-        Images: item.Images,
-        ItemInfo: item.ItemInfo,
-    };
-};
-
-const getRakutenUrl = async (
-    query: string,
-    rakutenItemCode?: string
-): Promise<string> => {
-    if (rakutenItemCode != null) {
+    itemCode?: string
+): Promise<RakutenItem> => {
+    if (itemCode != null) {
         const params = {
             applicationId: process.env.RAKUTEN_API_APPLICATION_ID as string,
             affiliateId: process.env.RAKUTEN_AFFILIATE_ID as string,
-            itemCode: rakutenItemCode,
+            itemCode: itemCode,
             hits: '1',
         };
         const urlSearchParams = new URLSearchParams(params).toString();
         const response = await fetch(
             `https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?${urlSearchParams}`
         );
-        return ((await response.json()) as any).Items[0].Item.affiliateUrl;
+        const item = ((await response.json()) as any).Items[0].Item;
+        return {
+            affiliateUrl: item.affiliateUrl,
+            itemName: item.itemName,
+            mediumImageUrls: item.mediumImageUrls,
+        };
     } else {
         const params = {
             applicationId: process.env.RAKUTEN_API_APPLICATION_ID as string,
@@ -109,40 +43,28 @@ const getRakutenUrl = async (
         const response = await fetch(
             `https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601?${urlSearchParams}`
         );
-        return ((await response.json()) as any).Items[0].Item.affiliateUrl;
+        const item = ((await response.json()) as any).Items[0].Item;
+        return {
+            affiliateUrl: item.affiliateUrl,
+            itemName: item.itemName,
+            mediumImageUrls: item.mediumImageUrls,
+        };
     }
 };
 
-const getYahooUrl = async (query: string, JAN?: string): Promise<string> => {
-    if (JAN != null) {
-        const params = {
-            appid: process.env.YAHOO_API_APP_ID as string,
-            affiliate_type: 'vc',
-            affiliate_id:
-                'https://ck.jp.ap.valuecommerce.com/servlet/referral?sid=3667930&pid=888917603&vc_url=',
-            jan_code: JAN,
-            results: '1',
-        };
-        const urlSearchParams = new URLSearchParams(params).toString();
-        const response = await fetch(
-            `https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?${urlSearchParams}`
-        );
-        return ((await response.json()) as any).hits[0].url;
-    } else {
-        const params = {
-            appid: process.env.YAHOO_API_APP_ID as string,
-            affiliate_type: 'vc',
-            affiliate_id:
-                'https://ck.jp.ap.valuecommerce.com/servlet/referral?sid=3667930&pid=888917603&vc_url=',
-            query: query,
-            results: '1',
-        };
-        const urlSearchParams = new URLSearchParams(params).toString();
-        const response = await fetch(
-            `https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?${urlSearchParams}`
-        );
-        return ((await response.json()) as any).hits[0].url;
-    }
+const getYahooUrl = async (query: string): Promise<string> => {
+    const params = {
+        appid: process.env.YAHOO_API_APP_ID as string,
+        affiliate_type: 'vc',
+        affiliate_id: `https://ck.jp.ap.valuecommerce.com/servlet/referral?sid=${process.env.VALUE_COMMERCE_SID}&pid=${process.env.VALUE_COMMERCE_PID}&vc_url=`,
+        query: query,
+        results: '1',
+    };
+    const urlSearchParams = new URLSearchParams(params).toString();
+    const response = await fetch(
+        `https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?${urlSearchParams}`
+    );
+    return ((await response.json()) as any).hits[0].url;
 };
 
 const Affiliates = async ({
@@ -154,70 +76,107 @@ const Affiliates = async ({
     asin?: string;
     rakutenItemCode?: string;
 }) => {
-    const amazonItem = await retry(async () => {
-        return await getAmazonItem(query, asin);
+    const rakutenItem = await retry(async () => {
+        return await getRakutenItem(query, rakutenItemCode);
     });
-    const amazonItemUrl = amazonItem.DetailPageURL;
-    const amazonItemImage = amazonItem.Images.Primary.Medium.URL;
-    const amazonItemInfo = amazonItem.ItemInfo;
-    const amazonItemTitle = amazonItemInfo.Title.DisplayValue;
-    const rakutenUrl = await retry(async () => {
-        return await getRakutenUrl(query, rakutenItemCode);
-    });
-    const JAN =
-        amazonItemInfo.ExternalIds != null
-            ? amazonItemInfo.ExternalIds.EANs.DisplayValues[0]
-            : undefined;
+    const rakutenUrl = rakutenItem.affiliateUrl;
+    const rakutenItemName = rakutenItem.itemName;
+    const rakutenImageUrl = rakutenItem.mediumImageUrls[0];
     const yahooUrl = await retry(async () => {
-        return await getYahooUrl(query, JAN);
+        return await getYahooUrl(query);
     });
-    return (
-        <div className={styles.card}>
-            <div className={styles.imageContainer}>
-                <Link href={amazonItemUrl}>
-                    <Image
-                        src={amazonItemImage}
-                        alt={amazonItemTitle}
-                        width={200}
-                        height={200}
-                        sizes="80vw"
-                        style={{
-                            borderRadius: '10px',
-                            width: '100%',
-                            height: 'auto',
-                        }}
-                    />
-                </Link>
-            </div>
-            <div className={styles.contentContainer}>
-                <Link href={amazonItemUrl}>
-                    <h2 className={styles.title}>
-                        {truncateTitle(amazonItemTitle)}
-                    </h2>
-                </Link>
-                <div className={styles.buttonGroup}>
-                    <Link
-                        href={amazonItemUrl}
-                        className={`${styles.button} ${styles.amazonButton}`}
-                    >
-                        Amazon
-                    </Link>
-                    <Link
-                        href={rakutenUrl}
-                        className={`${styles.button} ${styles.rakutenButton}`}
-                    >
-                        楽天
-                    </Link>
-                    <Link
-                        href={yahooUrl}
-                        className={`${styles.button} ${styles.yahooButton}`}
-                    >
-                        Yahoo!
+    if (asin != null) {
+        const amazonUrl = `https://www.amazon.co.jp/dp/${asin}/?ref=nosim?tag=${process.env.AMAZON_ASSOCIATE_PARTNER_TAG}`;
+        return (
+            <div className={styles.card}>
+                <div className={styles.imageContainer}>
+                    <Link href={amazonUrl}>
+                        <Image
+                            src={rakutenImageUrl}
+                            alt={rakutenItemName}
+                            width={200}
+                            height={200}
+                            sizes="80vw"
+                            style={{
+                                borderRadius: '10px',
+                                width: '100%',
+                                height: 'auto',
+                            }}
+                        />
                     </Link>
                 </div>
+                <div className={styles.contentContainer}>
+                    <Link href={amazonUrl}>
+                        <h2 className={styles.title}>
+                            {truncateTitle(rakutenItemName)}
+                        </h2>
+                    </Link>
+                    <div className={styles.buttonGroup}>
+                        <Link
+                            href={amazonUrl}
+                            className={`${styles.button} ${styles.amazonButton}`}
+                        >
+                            Amazon
+                        </Link>
+                        <Link
+                            href={rakutenUrl}
+                            className={`${styles.button} ${styles.rakutenButton}`}
+                        >
+                            楽天
+                        </Link>
+                        <Link
+                            href={yahooUrl}
+                            className={`${styles.button} ${styles.yahooButton}`}
+                        >
+                            Yahoo!
+                        </Link>
+                    </div>
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className={styles.card}>
+                <div className={styles.imageContainer}>
+                    <Link href={rakutenUrl}>
+                        <Image
+                            src={rakutenImageUrl}
+                            alt={rakutenItemName}
+                            width={200}
+                            height={200}
+                            sizes="80vw"
+                            style={{
+                                borderRadius: '10px',
+                                width: '100%',
+                                height: 'auto',
+                            }}
+                        />
+                    </Link>
+                </div>
+                <div className={styles.contentContainer}>
+                    <Link href={rakutenUrl}>
+                        <h2 className={styles.title}>
+                            {truncateTitle(rakutenItemName)}
+                        </h2>
+                    </Link>
+                    <div className={styles.buttonGroup}>
+                        <Link
+                            href={rakutenUrl}
+                            className={`${styles.button} ${styles.rakutenButton}`}
+                        >
+                            楽天
+                        </Link>
+                        <Link
+                            href={yahooUrl}
+                            className={`${styles.button} ${styles.yahooButton}`}
+                        >
+                            Yahoo!
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Affiliates;
