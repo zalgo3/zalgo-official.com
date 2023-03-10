a3ad201b13fb6155e5abb2f3caac77a6639d9932
zalgo / Hiroki Tanabe <33444773+zalgo3@users.noreply.github.com>
2023-03-10 14:06:48 +0900

    Update ui/affiliates.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- ui/affiliates.tsx
+++ ui/affiliates.tsx
@@ -1,8 +1,8 @@
 import retry from 'async-retry';
-import fetch from 'node-fetch';
 import {truncateTitle} from 'lib/string';
 import Image from 'next/image';
 import Link from 'next/link';
+import fetch from 'node-fetch';
 import styles from 'styles/ui/affiliates.module.css';
 
 type RakutenItem = {
