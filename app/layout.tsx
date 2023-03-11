ab0a00c6b35d9cfa35b6b13bfc4c3551093a2eab
zalgo / Hiroki Tanabe <33444773+zalgo3@users.noreply.github.com>
2023-03-12 03:57:39 +0900

    Add build and deploy to actions (#97)
    * Add build and deploy to actions
    
    * .
    
    * add env var
    
    * fix ci
    
    * fix ci
    
    * fix typo
    
    * fix ci
    
    * fix ci
    
    * fix typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import 'styles/global.css';
 
 import type {Metadata} from 'next';
+import Script from 'next/script';
 
 import Footer from './footer';
 
@@ -40,6 +41,24 @@ export const metadata: Metadata = {
 const RootLayout = ({children}: {children: React.ReactNode}) => {
     return (
         <html lang="ja">
+            <Script
+                id="gtag-base"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', 'G-KVTMLPFHK5');
+          `,
+                }}
+            />
+            <Script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3520947091484443"
+                crossOrigin="anonymous"
+            />
             <body>
                 {children}
                 <Footer />
