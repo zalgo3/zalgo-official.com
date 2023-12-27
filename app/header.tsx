9b92b31d659840e43793613b621e286240016f31
zalgo3 <zalgo3333@gmail.com>
2023-12-27 12:17:16 +0900

    update profile

--- app/header.tsx
+++ app/header.tsx
@@ -2,15 +2,15 @@ import Link from 'next/link';
 import styles from 'styles/app/header.module.css';
 
 const Header = () => {
-    return (
-        <header className={styles.header}>
-            <h1 className={styles.logo}>
-                <Link href="/" className={styles.logoLink}>
-                    ざるご / 田辺広樹
-                </Link>
-            </h1>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.logo}>
+        <Link href="/" className={styles.logoLink}>
+          ざるご / 田辺広樹
+        </Link>
+      </h1>
+    </header>
+  );
 };
 
 export default Header;
