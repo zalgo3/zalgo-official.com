7ac154c16c6f839c99c3a2c3f852f03556c67ed6
zalgo3 <zalgo3333@gmail.com>
2023-12-27 14:35:04 +0900

    fix youtube link

--- app/blog/social-links.tsx
+++ app/blog/social-links.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { FaUser,FaYoutube } from 'react-icons/fa';
+import { FaUser, FaYoutube } from 'react-icons/fa';
 import { FaXTwitter } from "react-icons/fa6";
 import styles from 'styles/app/social-links.module.css';
 
@@ -16,7 +16,7 @@ const SocialLinks = () => {
       <span>ゲーム</span>
     </a>
     <a
-      href="https://music.youtube.com/channel/UCD3YZCdQH58hVbw4krMrQjw?sub_confirmation=1"
+      href="https://www.youtube.com/channel/UCD3YZCdQH58hVbw4krMrQjw?sub_confirmation=1"
       target="_blank"
       rel="noopener noreferrer"
     >
