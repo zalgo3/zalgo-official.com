54abed1345550d34e17db2d41b93a0ebd7b781f0
zalgo / Hiroki Tanabe <33444773+zalgo3@users.noreply.github.com>
2024-06-29 22:53:41 +0900

    use @next/third-parties for google tag manager (#447)
    * use @next/third-parties for google tag manager
    
    * Update app/layout.tsx
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * yarn upgrade
    
    * update github actions
    
    * use node 20
    
    * Revert "use node 20"
    
    This reverts commit 1b645690957b9e5e353525b59a5cd853aec61ac5.
    
    * .
    
    * remove jest-coverage-report
    
    * use node 20
    
    * yarn v4
    
    * Revert "yarn v4"
    
    This reverts commit 576563b8b37fa0291ea7c43a5790a05fa4a606da.
    
    * Revert "use node 20"
    
    This reverts commit 87d7910a996f3550fd3f2e8cd8e89055aadf477e.
    
    * Revert "remove jest-coverage-report"
    
    This reverts commit cf59d9fd0b00436b8cee1ef49379c257fb2b5963.
    
    * Revert "."
    
    This reverts commit 10c960ae48532c9205faa1625d09765c915c54b5.
    
    * use npm
    
    * fix
    
    * remove coverage report
    
    * fix option
    
    * fix netlify.toml
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import 'styles/global.css';
 
+import {GoogleTagManager} from '@next/third-parties/google';
 import type {Metadata} from 'next';
 import Script from 'next/script';
 
@@ -40,19 +41,7 @@ export const metadata: Metadata = {
 const RootLayout = ({children}: {children: React.ReactNode}) => {
     return (
         <html lang="ja">
-            <Script
-                id="gtag-base"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer', 'G-KVTMLPFHK5');
-          `,
-                }}
-            />
+            <GoogleTagManager gtmId="G-KVTMLPFHK5" />
             <Script
                 async
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3520947091484443"
