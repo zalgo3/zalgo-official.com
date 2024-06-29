39f037b1f118ca0c90012e46f92b53539c1495c2
zalgo3 <zalgo3333@gmail.com>
2024-06-30 01:32:10 +0900

    migrate to eslint flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("next/core-web-vitals", "prettier"), {
+    plugins: {
+        "simple-import-sort": simpleImportSort,
+        import: fixupPluginRules(_import),
+    },
+
+    rules: {
+        "simple-import-sort/imports": "error",
+        "simple-import-sort/exports": "error",
+        "import/first": "error",
+        "import/newline-after-import": "error",
+        "import/no-duplicates": "error",
+    },
+}];
