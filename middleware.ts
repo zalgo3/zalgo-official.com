807b80fa1afb4e08f8ce17a204fd5ac5ac4f85ca
zalgo3 <zalgo3333@gmail.com>
2025-04-10 01:40:40 +0900

    use middleware

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const slugMatch = pathname.match(/^\/([^/]+)$/);
+
+  if (slugMatch && slugMatch[1] !== 'blog') {
+    const slug = slugMatch[1];
+    const redirectUrl = new URL(`/blog/${slug}`, request.url);
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+}
