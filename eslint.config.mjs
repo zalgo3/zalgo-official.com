9aeb27407b020ec3312aaa941e9e77a4de692564
zalgo3 <zalgo3333@gmail.com>
2025-04-12 13:30:23 +0900

    fix rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,8 +36,6 @@ export default tseslint.config(
         },
         rules: {
             '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-            '@typescript-eslint/no-unsafe-assignment': 'off',
-            '@typescript-eslint/no-misused-promises': 'off',
         },
     },
     {
