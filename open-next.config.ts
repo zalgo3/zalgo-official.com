39d72640a6320e1e40ac451dd0298cc95ec91aed
zalgo3 <zalgo3333@gmail.com>
2026-02-15 01:51:34 +0900

    fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,8 +1,3 @@
-// @ts-nocheck
-// default open-next.config.ts file created by @opennextjs/cloudflare
 import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
-import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-export default defineCloudflareConfig({
-	incrementalCache: r2IncrementalCache,
-});
+export default defineCloudflareConfig();
