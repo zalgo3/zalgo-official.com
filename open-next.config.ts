6e19328c8eb966371ebee5f78a83a6dc4b28a8ac
zalgo3 <zalgo3333@gmail.com>
2026-02-15 01:10:07 +0900

    fix

--- open-next.config.ts
+++ open-next.config.ts
@@ -3,6 +3,19 @@ import type { OpenNextConfig } from "@opennextjs/cloudflare";
 const config: OpenNextConfig = {
   default: {
     runtime: "edge",
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
   },
   buildCommand: "npm run build:next",
 };
