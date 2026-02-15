68e742ec3f225657fc0e2d678ef51ccc4ff1563c
zalgo3 <zalgo3333@gmail.com>
2026-02-15 19:35:49 +0900

    middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import type {NextRequest} from 'next/server';
 import {NextResponse} from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
     const {pathname} = request.nextUrl;
 
     const slugMatch = /^\/([^/]+)$/.exec(pathname);
